1054d00ac3e8792fd3001d9fd6e28d07ad843ca7
AlvaroTF-git <alvaro.tferreir4@gmail.com>
2025-09-06 13:22:22 +0000

    programa2.0

--- cadastro_cartas.py
+++ cadastro_cartas.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 def cadastro_cartas():
     codigo = str(input('Digite o código da sua carta, EX -> A99\nCÓDIGO: ')).strip()
     estado = str(input('Digite a sigla do Estado, EX -> PE\nESTADO: ')).strip()
@@ -19,7 +21,57 @@ def exibir_carta(nickname, codigo, estado, populacao, pontos_turisticos):
     print(f'CÓDIGO: {codigo}')
     print(f'ESTADO: {estado}')
     print(f'POPULAÇÃO: {populacao}\nPontos TURÍSTICOS: {pontos_turisticos}')
+    print("=-="*20)
+
+def interface_main():
+    print("Opções do Programa: ")
+    print("1. começar o jogo")
+    print("2. encerrar o programa")
+    opcao = int(input("Digite uma opção: "))
+    return opcao
 
 
+def interface_comparacao():
+    print("Escolha um atributo para comparar: ")
+    print(f"1. População")
+    print(f"2. Pontos Turísticos")
+    inter_comparacao = int(input("Digite um número: "))
+    return inter_comparacao
 
-    
+def comparacao(escolha_atributo, carta1, carta2):
+    if escolha_atributo == 1:
+        crit = 2
+        sleep(0.5)
+        print("==="*13)
+        print("A comparação escolhida é: população!!")
+        print(f"População da primeira carta --> {carta1[crit]}\nPopulação da segunda carta --> {carta2[crit]} ")
+        print("==="*13)
+        sleep(0.5)
+        if carta1[crit] > carta2[crit]:
+            print("### Jogador 1 venceu ###")
+        elif carta1[crit] < carta2[crit]:
+            print("### Jogador 2 venceu! ###")
+        else:
+            print("### Os jogadores empataram!! ###")
+    if escolha_atributo == 2:
+        crit = 3
+        sleep(0.5)
+        print("==="*13)
+        print("A comparação escolhida é: Pontos Turísticos!!")
+        print(f"População da primeira carta --> {carta1[crit]}\nPopulação da segunda carta --> {carta2[crit]} ")
+        print("==="*13)
+        sleep(0.5)
+        if carta1[crit] > carta2[crit]:
+            print("### Jogador 1 venceu ###")
+        elif carta1[crit] < carta2[crit]:
+            print("### Jogador 2 venceu! ###")
+        else:
+            print("### Os jogadores empataram!! ###")
+
+
+def interface_game():
+    print("1. Comparar atributo das cartas")
+    print("2. Mostrar as cartas")
+    print("3. Encerrar o programa")
+    option = int(input("Digite a opção escolhida: "))
+    return option
--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from cadastro_cartas import(cadastro_jogador, cadastro_cartas, exibir_carta)
+from cadastro_cartas import(cadastro_jogador, cadastro_cartas, exibir_carta, interface_main, interface_comparacao, comparacao,interface_game)
 from time import sleep
 
-
+game = 1
 print("=-="*20)
 print("##  Bem Vindo ao Jogo: Super-Trunfo  ##")
 print("=-="*20)
-jogador1 = cadastro_jogador()
-sleep(0.5)
-carta1 = cadastro_cartas()
-exibir_carta(jogador1, *carta1)
+opcao = interface_main()
+if opcao == 1:
+        ##cadastro do primeiro jogador
+        print("--- Cadastrando o primeiro jogador ---")
+        jogador1 = cadastro_jogador()
+        sleep(0.5)
+        carta1 = cadastro_cartas()
+        exibir_carta(jogador1, *carta1)
+        sleep(0.5)
+        ##cadastro do segundo jogador jogador
+        print("--- Cadastrando o Segundo jogador ---")
+        jogador2 = cadastro_jogador()
+        sleep(0.5)
+        carta2 = cadastro_cartas()
+        exibir_carta(jogador2, *carta2)
+elif opcao == 2:
+        print("Encerrando o programa...")
+
+while True:
+    escolha = interface_game()
+    if escolha == 1:
+        escolha_atributo = interface_comparacao()
+        if escolha_atributo == 1:
+              comparacao(escolha_atributo, carta1, carta2)
+              break
+        if escolha_atributo == 2:
+              comparacao(escolha_atributo, carta1, carta2)
+              break
+    elif escolha == 2:
+        exibir_carta(jogador1, carta1[0], carta1[1],carta1[2],carta1[3])
+        exibir_carta(jogador2, carta2[0], carta2[1],carta2[2],carta2[3])
+        sleep(10)
+     
+print("Encerrando o programa...")
+              
